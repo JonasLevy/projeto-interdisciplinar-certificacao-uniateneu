881cb1ea7b3ea196fb096d2f6e47a1e10903ffeb
Jonas Levy <jonaslevy0408@gmail.com>
2025-09-22 12:04:09 -0300

    criei o modal na pagina delivery para ser usado como exemplo

--- src/pages/moradores/DeliveryMorador.jsx
+++ src/pages/moradores/DeliveryMorador.jsx
@@ -1,15 +1,33 @@
-import { Fab } from '@mui/material';
+import { Button, Fab, TextField } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonModal from '../../componets/ButtonModal';
+import BasicModal from '../../componets/Modal';
 
 const DeliveryMorador = () => {
-    return (
-        <div className="min-h-full w-full "> 
-            <h1>Delivery</h1>
-            <ButtonModal />
-        </div>
-    );
-};
+    const [openModal, setOpenModal] = useState(false);
 
-export default DeliveryMorador;
+    const submitForm = (e) => {
+        e.preventDefault();
+        // Lógica para enviar o formulário
+        setOpenModal(false);
+    }
+
+        return (
+            <div className="min-h-full w-full ">
+                <h1>Delivery</h1>
+                <ButtonModal click={() => setOpenModal(true)} />
+                <BasicModal openModal={openModal} title="Novo Pedido" close={() => setOpenModal(false)}>
+                    <form onSubmit={submitForm} className="flex flex-col gap-4">
+                        <TextField id="outlined-basic" label="Nome" variant="outlined" />
+                        <TextField id="outlined-basic" label="email" variant="outlined" />
+                        <Button variant="contained" type='submit'>Confirmar</Button>
+
+                    </form>
+                </BasicModal>
+
+            </div>
+        );
+    };
+
+    export default DeliveryMorador;
