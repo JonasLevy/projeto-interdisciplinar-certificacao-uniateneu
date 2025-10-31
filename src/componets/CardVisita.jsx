a11443ba6947d2456e0ed354e0ca2e5d2cd1ed47
Pedro Reis <126337464+P3droReis@users.noreply.github.com>
2025-10-31 19:25:22 -0300

    FormVisita Componente Criado

--- src/componets/CardVisita.jsx
+++ src/componets/CardVisita.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-const CardVisita = () => {
+const CardVisita = ({clickEditar}) => {
+    
+    const OnClickDetalhe = (id) => {
+        clickEditar(id);
+    }
+    
     return (
         <article className="border p-4 rounded shadow-md bg-white">
             <h2 className="text-lg font-semibold mb-2">Visita</h2>
@@ -8,8 +13,16 @@ const CardVisita = () => {
             <p className="text-gray-600 text-xs">Nome: Fulano de Tal</p>
             <p className="text-gray-600 text-xs">Data: 01/01/2024</p>
             <p className="text-gray-600 text-xs">Hora de Entrada: 14:00</p>
-            <p className="text-gray-600 text-xs">Hora de Saída: 15:00</p>
             <p className="text-gray-600 text-xs">Apartamento: 101</p>
+
+            <div className="flex justify-end ">
+                <button
+                    className="px-3 py-1 bg-blue-600 text-white text-sm rounded hover:bg-blue-700"
+                    onClick={() => OnClickDetalhe("123")}
+                >
+                    Editar
+                </button>
+            </div>
         </article>
     );
 };
