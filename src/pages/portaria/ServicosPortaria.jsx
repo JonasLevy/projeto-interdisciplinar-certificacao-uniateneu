75c976598b105359531d1ac13e77fe72bceeef16
Anderson Mesquita <andersomdbjogos@gmail.com>
2025-11-12 09:16:22 -0300

    Adicao das telas da portaria

--- src/pages/portaria/ServicosPortaria.jsx
+++ src/pages/portaria/ServicosPortaria.jsx
@@ -7,6 +7,61 @@ import CardServico from '../../componets/CardServico';
 import FormSevico from '../../componets/FormSevico';
 
 const ServicosMorador = () => {
+    const [openModal, setOpenModal] = useState(false);
+
+    const [tipoModal, setTipoModal] = useState(null);
+
+    const [servico, setServico] = useState(null);
+
+    const clickOpenModal = () => {
+        setTipoModal("Criar");
+        setOpenModal(!openModal);
+    }
+
+    const clickEditar = (idReserva) => {
+        setTipoModal("Editar");
+        setOpenModal(!openModal);
+    }
+
+    return (
+        <div className="min-h-full w-full ">
+            <div
+                className='flex  h-16 bg-slate-300 p-3 items-center justify-between'
+            >
+                <h1>Serviços</h1>
+                <div className='flex gap-1'>
+                    <TextField
+                        id="outlined-basic"
+                        label="Apartamento"
+                        variant="outlined"
+                        size='small'
+                    //value={apt}
+                    // onChange={(e) => setApt(e.target.value)}
+                    />
+                    <Button variant="contained" aria-label="search" size='small' color='success'>
+                        <SearchIcon />
+                    </Button>
+                </div>
+            </div>
+
+            <section className='p-8'>
+                <CardServico clickEditar={() => clickEditar()} />
+
+            </section>
+            <ButtonModal click={() => clickOpenModal()} tipoModal={tipoModal} />
+            <BasicModal 
+                openModal={openModal} 
+                title={`${tipoModal} Servico`} 
+                close={() => setOpenModal(false)}>
+                <FormSevico
+                    tipoUsuario={"Portaria"}
+                    criarServico={servico}
+                    criarOuEditar={tipoModal}
+                    fecharModal={() => setOpenModal(!openModal)}
+                />
+            </BasicModal >
+        </div>
+    );
 };
 
 export default ServicosMorador;
--- src/pages/portaria/VisitasPortaria.jsx
+++ src/pages/portaria/VisitasPortaria.jsx
@@ -8,6 +8,58 @@ import FormVisita from '../../componets/FormVisita';
 
 
 const VisitasMorador = () => {
+    const [openModal, setOpenModal] = useState(false);
+
+    const [tipoModal, setTipoModal] = useState(null); // Criar ou Editar
+
+    const clickOpenModal = () => {
+        setTipoModal("Criar");
+        setOpenModal(!openModal);
+    }
+
+    const clickEditar = (idReserva) => {
+        setTipoModal("Editar");
+        setOpenModal(!openModal);
+    }
+
+    return (
+        <div className="min-h-full w-full ">
+            <div
+                className='flex  h-16 bg-slate-300 p-3 items-center justify-between'
+            >
+                <h1>Visitas</h1>
+                <div className='flex gap-1'>
+                    <TextField
+                        id="outlined-basic"
+                        label="Apartamento"
+                        variant="outlined"
+                        size='small'
+                    //value={apt}
+                    // onChange={(e) => setApt(e.target.value)}
+                    />
+                    <Button variant="contained" aria-label="search" size='small' color='success'>
+                        <SearchIcon />
+                    </Button>
+                </div>
+            </div>
+            <section className='p-8'>
+                <CardVisita clickEditar={() => clickEditar()}  />
+            </section>
+
+            <ButtonModal click={() => clickOpenModal()} tipoModal={tipoModal} />
+
+            <BasicModal 
+                openModal={openModal} 
+                title={`${tipoModal} Visita`}
+                close={() => setOpenModal(false)}>
+
+                <FormVisita
+                    tipoUsuario={"Portaria"}
+                    criarOuEditar={tipoModal}
+                    fecharModal={() => setOpenModal(!openModal)} />
+            </BasicModal>
+        </div>
+    );
 };
 
 export default VisitasMorador;
